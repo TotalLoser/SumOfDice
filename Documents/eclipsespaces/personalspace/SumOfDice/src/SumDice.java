26ed631c3992e4641bf2f984e6c153d0e57a5867
TotalLoser <selloutmillionare@gmail.com>
2018-02-23 11:49:14 -0700

    Update SumDice.java

--- Documents/eclipsespaces/personalspace/SumOfDice/src/SumDice.java
+++ Documents/eclipsespaces/personalspace/SumOfDice/src/SumDice.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
+//test
 public class SumDice
 {
 	public static void main(String[] args)
